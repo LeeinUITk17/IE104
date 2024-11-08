75bc1d42ee147c40ba61a41e83e9af3d1920b0d6
LeeinUITk17 <22521276@gm.uit.edu.vn>
2024-11-08 09:39:24 +0700

    feat: init-weekly-assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+
+  // document.getElementById('login').addEventListener('submit', (e) => {
+  //   e.preventDefault(); 
+  //   const username = document.getElementById('username').value;
+  //   alert(`Login successful ${username}`);
+  // });
+
+  
